dc95f74c975e74164d9798a95d3b4e3013bf6800
robinstrunk <robin.strunk@rwth-aachen.de>
2018-09-21 18:49:46 +0200

    Section Two, Video Three

--- app/src/main/java/com/greemand/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/greemand/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.greemand.msgshareapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,14 @@ class MainActivity : AppCompatActivity() {
             //Code
             Log.i("MainActivity", "Button Clicked!")
             Toast.makeText(this, "Button Clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMessageToNextActivity.setOnClickListener {
+
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
