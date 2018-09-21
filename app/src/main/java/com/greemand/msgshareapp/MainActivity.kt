d3cd0c5a48a6c5793734490bd9becbd8f444ed2b
robinstrunk <robin.strunk@rwth-aachen.de>
2018-09-21 18:02:57 +0200

    Section Two, Video Two

---
+++ app/src/main/java/com/greemand/msgshareapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.greemand.msgshareapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            //Code
+            Log.i("MainActivity", "Button Clicked!")
+            Toast.makeText(this, "Button Clicked!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
