538d96386186527d3fc8be28f754c14aceccb6a4
robinstrunk <robin.strunk@rwth-aachen.de>
2018-09-22 14:44:20 +0200

    Section Two, Video Five

--- app/src/main/java/com/greemand/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/greemand/msgshareapp/MainActivity.kt
@@ -22,9 +22,23 @@ class MainActivity : AppCompatActivity() {
         btnSendMessageToNextActivity.setOnClickListener {
 
             val message: String = etUserMessage.text.toString()
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
             val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("user_message", message)
+
             startActivity(intent)
+
+
+        }
+
+        btnShareToOtherApps.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/Plain"
+
+            startActivity(Intent.createChooser(intent, "Share message to: "))
         }
     }
 }
