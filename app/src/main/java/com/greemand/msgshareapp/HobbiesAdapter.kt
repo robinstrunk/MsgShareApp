3893130ac4fecb1ae94da2636733e27b37664419
robinstrunk <robin.strunk@rwht-aachen.de>
2018-09-25 16:20:34 +0200

    First commit from Desktop. Increased Kotlin Version in build.gradle by 1. Finished section three

--- app/src/main/java/com/greemand/msgshareapp/HobbiesAdapter.kt
+++ app/src/main/java/com/greemand/msgshareapp/HobbiesAdapter.kt
@@ -1,10 +1,15 @@
 package com.greemand.msgshareapp
 
 import android.content.Context
+import android.content.Intent
+import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.greemand.msgshareapp.R.id.etUserMessage
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.list_item.view.*
 
 class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>) : RecyclerView.Adapter<HobbiesAdapter.MyViewHolder>() {
@@ -23,8 +28,31 @@ class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>) : RecyclerV
     }
 
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var currentHobby: Hobby? = null
+        var currentPosition: Int = 0
+
+        init {
+            itemView.setOnClickListener {
+                Toast.makeText(context, currentHobby!!.title + " Clicked!", Toast.LENGTH_SHORT).show()
+            }
+
+            itemView.imgShare.setOnClickListener{
+                val message: String = "My hobby is: " + currentHobby!!.title
+                val intent = Intent()
+                intent.action = Intent.ACTION_SEND
+                intent.putExtra(Intent.EXTRA_TEXT, message)
+                intent.type = "text/Plain"
+
+                context.startActivity(Intent.createChooser(intent, "Share message to: "))
+
+            }
+        }
         fun setData(hobby: Hobby?, position: Int) {
             itemView.txvTitle.text = hobby!!.title
+
+            this.currentHobby = hobby
+            this.currentPosition = position
         }
     }
 
